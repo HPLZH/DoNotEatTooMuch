790d7f742d662bf505a5818ef65c8c4182fbfdf1
李子晗 <lizihan@hplzh.cn>
2023-04-02 01:26:56 +0800

    补充注释(wurt)

--- scripts/strings/cn/speech_wurt.lua
+++ scripts/strings/cn/speech_wurt.lua
@@ -1,5 +1,6 @@
 return {
     ANNOUNCE_EAT = {
+        -- Author : Fu Jiongwen
         MOD_DO_NOT_EAT_TOO_MUCH_FULL = "似乎吃得有点多了，浮浪噗。",
         MOD_DO_NOT_EAT_TOO_MUCH_OVERFLOW = "不能吃更多了，浮浪噗。",
         MOD_DO_NOT_EAT_TOO_MUCH_DANGER = "格浪噗，感觉肚子要爆炸了。"
