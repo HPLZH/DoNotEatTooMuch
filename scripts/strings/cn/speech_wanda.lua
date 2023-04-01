9d83bbad4049d7bd88328074720843bb3829a5c3
李子晗 <lizihan@hplzh.cn>
2023-04-01 23:46:56 +0800

    修改wanda的台词

--- scripts/strings/cn/speech_wanda.lua
+++ scripts/strings/cn/speech_wanda.lua
@@ -1,7 +1,7 @@
 return {
     ANNOUNCE_EAT = {
         -- Author : Fu Jiongwen
-        MOD_DO_NOT_EAT_TOO_MUCH_FULL = "我记得刚才已经吃饱过了。",
+        MOD_DO_NOT_EAT_TOO_MUCH_FULL = "我记得我上次吃饱还是在上次。",
         MOD_DO_NOT_EAT_TOO_MUCH_OVERFLOW = "我不能再把时间浪费在吃上了。",
         MOD_DO_NOT_EAT_TOO_MUCH_DANGER = "我必须得暂时放下吃东西的念头。"
     }
