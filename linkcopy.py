a584a4b19440b8ac301019eac2445be73cd0715b
李子晗 <lizihan@hplzh.cn>
2023-04-01 22:19:00 +0800

    修正发布工具linkcopy

--- linkcopy.py
+++ linkcopy.py
@@ -1,7 +1,7 @@
 import sys
 infiles = sys.argv[1:-1]
 outfile = sys.argv[-1]
-fo = open(outfile, encoding="utf-8")
+fo = open(outfile, "w", encoding="utf-8")
 for fn in infiles:
     f = open(fn, encoding="utf-8")
     fo.write(f.read())
